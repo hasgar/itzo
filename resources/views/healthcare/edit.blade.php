249f81f6bf8744f88e040ec06de8db6ffd3b3f59
Hasgar <hasgardee@gmail.com>
2016-10-15 00:20:53 +0530

    updates

--- resources/views/healthcare/edit.blade.php
+++ resources/views/healthcare/edit.blade.php
@@ -10,70 +10,15 @@
 
 
 		
-		<div class="md-overlay"></div> <!-- Overlay for Popup -->
+	<div class="md-overlay"></div> <!-- Overlay for Popup -->
 							<div id="menu">
-								<ul>
-									<li><a href="/">Home</a>
-									</li>
-									@if (Auth::guest())
-									<li><a href="/signin">Sign In</a> / <a href="/signup">Sign Up</a></li>
-									</li>
-									@else 
-									@if(Sentinel::findById(Auth::user()->id)->inRole('user'))
-									<li><a href="/user/dashboard">Dashboard</a></li>
-									</li>
-									@endif
-									@if(Sentinel::findById(Auth::user()->id)->inRole('healthcare'))
-									<li><a href="/healthcare/dashboard">  Dashboard</a></li>
-									</li>
-									@endif
-									@if(Sentinel::findById(Auth::user()->id)->inRole('admin'))
-									<li><a href="/admin/dashboard">  Dashboard</a></li>
-									</li>
-									@endif
-									@endif
-
-								</ul>
+								@include('public.layouts.headerMob')
 							</div>
 		<div class="lp-menu-bar  lp-menu-bar-color">
 			<div class="container">
 					<div class="row">
-						@include('public.layouts.logo')
-						<div class="col-xs-6 mobile-nav-icon">
-							<a href="#menu" class="nav-icon">
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</a>s
-						</div>
-						<div class="col-md-8 col-xs-12 lp-menu-container">
-							<div class="pull-right lp-add-listing-btn">
-								<ul>
-
-									<li><a href="/add-health-care"><i class="fa fa-plus"></i> Add your health care service</a></li>
-								</ul>
-							</div>
-							<div class="lp-menu pull-right menu">
-								<ul>
-									@if (Auth::guest())
-									<li><a href="/signin"><i class="fa fa-user user-plus-icon"></i>  Login</a> / <a href="/signup"><i class="fa fa-user-plus user-plus-icon"></i>  Register</a></li>
-									@else 
-									@if(Sentinel::findById(Auth::user()->id)->inRole('user'))
-									<li><a href="/user/dashboard"><i class="fa fa-dashboard user-plus-icon"></i>  Dashboard</a></li>
-									</li>
-									@endif
-									@if(Sentinel::findById(Auth::user()->id)->inRole('healthcare'))
-									<li><a href="/healthcare/dashboard"><i class="fa fa-dashboard user-plus-icon"></i>  Dashboard</a></li>
-									</li>
-									@endif
-									@if(Sentinel::findById(Auth::user()->id)->inRole('admin'))
-									<li><a href="/admin/dashboard"><i class="fa fa-dashboard user-plus-icon"></i>  Dashboard</a></li>
-									</li>
-									@endif
-									@endif
-								</ul>
-							</div>
-						</div>
+					@include('public.layouts.logo')
+						
 					</div>
 				</div>
 		</div><!-- ../menu-bar -->
