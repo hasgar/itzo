2a111f27e7f6e1b14a90fa34cf71cdaa7c886468
Hasgar D <hasgardee@gmail.com>
2017-03-06 07:15:04 +0530

    commit

--- resources/views/admin/healthcares.blade.php
+++ resources/views/admin/healthcares.blade.php
@@ -135,7 +135,7 @@
 														</div> -->
 
 														<div class="lp-list-pay-btn">
-															@if($healthcare['payment_done'] != 1)  <a href="/admin/healthcare/paymentDone/{{$healthcare['id']}}/{{urlencode($healthcare['name'])}}" onclick="return confirm('Are you sure?')"> <i class="fa fa-close"></i>
+															@if($healthcare['payment_done'] == 0)  <a href="/admin/healthcare/paymentDone/{{$healthcare['id']}}/{{urlencode($healthcare['name'])}}" onclick="return confirm('Are you sure?')"> <i class="fa fa-close"></i>
 																<span>Payment Done</span>
 															  </a>
 															  @endif
@@ -144,7 +144,7 @@
 
 														<div class="lp-list-pay-btn">
 															@if($healthcare['is_approved'] == 1)  <a href="/admin/healthcare/block/{{$healthcare['id']}}/{{urlencode($healthcare['name'])}}" onclick="return confirm('Are you sure?')"> <i class="fa fa-close"></i>
-																<span>Blocks</span>
+																<span>Block</span>
 															  </a> @else <a href="/admin/healthcare/approve/{{$healthcare['id']}}/{{urlencode($healthcare['name'])}}" onclick="return confirm('Are you sure?')"> <i class="fa fa-check"></i>
 																<span>Approve</span>
 															  </a>
