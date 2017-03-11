6fdab52e5accdef3664ea3fa6733f1f8ce7b044a
Hasgar D <hasgardee@gmail.com>
2017-03-11 22:14:29 +0530

    commit

--- resources/views/public/layouts/footerScripts.blade.php
+++ resources/views/public/layouts/footerScripts.blade.php
@@ -27,18 +27,7 @@
 <script>
 var isub = 0;
 $(".form-horizontal").validate({
-  invalidHandler: function(event, validator) {
-    // 'this' refers to the form
-    var errors = validator.numberOfInvalids();
-    if (errors) {
-      var message = errors == 1
-        ? 'You missed 1 field. It has been highlighted'
-        : 'You missed ' + errors + ' fields. They have been highlighted';
-      alert(message);
-
-    }
-
-  },
+	
 	submitHandler: function(form){
 		isub++;
 		if (isub > 1) {
