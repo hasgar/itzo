6e3dd7eb1fbc5f57c1e217839d7f96321316213b
Hasgar D <hasgardee@gmail.com>
2017-04-08 07:21:33 +0530

    dsdsd

--- resources/views/admin/healthcares.blade.php
+++ resources/views/admin/healthcares.blade.php
@@ -73,7 +73,7 @@
 
 							<div class="user-recent-listings-container">
 									<div class="col-md-8"><h3 class="padding-top-35 padding-bottom-35">All Healthcares</h3></div><div class="col-md-4"><div class="price-plan-button  pull-right">
-									<a href="/logout" class="lp-secondary-btn btn-second-hover">Sign Out</a>
+									<a href="/logout" class="lp-default-btn btn-second-hover">Add Healthcare</a>   <a href="/logout" class="lp-secondary-btn btn-second-hover">Sign Out</a>
 								</div></div><div class="user-recent-listings-inner">
 									<div class="row lp-list-page-list">
 									@if(count($healthcares) > 0)
